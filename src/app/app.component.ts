fd78225ce5c918b85f491d87b01323d0e0495b25
edjeezy <Edji@DESKTOP-QT9FD5I>
2022-10-24 20:06:59 +0000

    little demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crm_dashboard';
+  showMeeting() {
+    this.title = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, obcaecati? Omnis aperiam distinctio eveniet alias ipsa! Nisi harum totam ad reprehenderit sunt architecto voluptas eos libero? Ab recusandae totam ullam!';
+  }
+
+  hideMeeeting() {
+    this.title = 'nothing selected';
+  }
 }
